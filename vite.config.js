9a5dae5e8b23916b0fc649b824f547a4a43c5bdc
Filip Jirsák <filip@jirsak.org>
2025-05-09 23:18:00 +0200

    chore: Aktualizace na Vite 6 a React 19.1

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: './src',
+  root: 'src/',
   publicDir: '../public',
   server: {
     hmr: false,
   },
   build: {
     outDir: '../dist',
-    modulePreload: false,
+    emptyOutDir: true,
+    target: 'es2022',
+    inputGlobPatterns: ['src/pages/**/*.html'],
+    modulePreload: {
+      resolveDependencies: false,
+    },
   },
   plugins: [react()],
 });
